60efe492cb789b0699b9c1e6e5863899e9e5c095
Sean Harte <scsharte@gmail.com>
2018-03-13 19:05:51 -0400

    fully add authentication

--- server.js
+++ server.js
@@ -1,17 +1,51 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
-//using morgan to make sure server logs are standarized
-const morgan = require('morgan');
 //using mongoose for connecting to the server
 const mongoose = require('mongoose');
+//using morgan to make sure server logs are standarized
+const morgan = require('morgan');
+const passport = require('passport');
 
-const app = express();
 
+const { router: usersRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+
+mongoose.Promise = global.Promise;
 //bring over Port & DATABASE_URL from config.js
 const { PORT, DATABASE_URL } = require('./config');
 
-const userRouter = require('./userrouter.js');
+const app = express();
+
+// Logging
+app.use(morgan('common'));
+
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/users/', usersRouter);
+app.use('/api/auth/', authRouter);
+
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+// A protected endpoint which needs a valid JWT to access it
+app.get('/api/protected', jwtAuth, (req, res) => {
+  return res.json({
+    data: 'rosebud'
+  });
+});
 
 app.use(express.static('public'));
 
@@ -22,6 +56,11 @@ if (require.main === module) {
     console.info(`App listening on ${this.address().port}`);
   });
 }
+
+//if not the any useable end point then display a message
+app.use('*', function (req, res) {
+  res.status(404).json({ message: 'Page Not Found' });
+});
 
 //the variable server is blank but will be assigned under runServer and will be used again in closerserver
 let server;
@@ -61,13 +100,5 @@ function closeServer() {
   });
 }
 
-//if not the any useable end point then display a message
-app.use('*', function (req, res) {
-  res.status(404).json({ message: 'Page Not Found' });
-});
-
-if (require.main === module) {
-  runServer(DATABASE_URL).catch(err => console.error(err));
-}
 
 module.exports = { app, runServer, closeServer };
