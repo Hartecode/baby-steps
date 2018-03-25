beb0258de2f6c59e186329496fd5518d167fb1b4
Sean Harte <scsharte@gmail.com>
2018-03-25 13:33:39 -0400

    updated the authoization to return a jwt and user id

--- auth/router.js
+++ auth/router.js
@@ -20,7 +20,8 @@ router.use(bodyParser.json());
 // The user provides a username and password to login
 router.post('/login', localAuth, (req, res) => {
   const authToken = createAuthToken(req.user.serialize());
-  res.json({authToken});
+  res.json({authToken, userId: req.user._id});
+  // res.json({authToken});
 });
 
 const jwtAuth = passport.authenticate('jwt', {session: false});
