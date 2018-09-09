47e24b17f6ab0f3289cb763435df709cbe9723ba
Sean Harte <scsharte@gmail.com>
2018-09-09 09:38:55 -0400

    switched around the ports

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ exports.TEST_DATABASE_URL =
 	process.env.TEST_DATABASE_URL || 
 	'mongodb://localhost/test-baby-steps';
 
-exports.PORT =  8080 || process.env.PORT;
+exports.PORT =  process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
